503d706b96aa5ef44c59718b8bdb47dae6af5913
Nebojša Bogosavljev <nebojsa253@gmail.com>
2023-01-26 23:28:04 +0100

    feature 3.12 fix

--- ISA-projekat/src/main/java/com/isa/domain/model/Poll.java
+++ ISA-projekat/src/main/java/com/isa/domain/model/Poll.java
@@ -9,126 +9,79 @@ import javax.persistence.OneToOne;
 @Entity
 public class Poll extends AbstractEntity {
 
-//    private BloodType bloodType;
-//
-//    private boolean patientArrived;
-//
-//    private boolean deniedType;
-//
-//    private String reasonForDenied;
-//
-//    private double amountOfBlood;
-//
-//    private boolean weight;
-//
-//    private boolean sickness;
-//
-//    private boolean infection;
-//
-//    private boolean pressure;
-//
-//    private boolean therapy;
-//
-//    private boolean cycle;
-//
-//    private boolean dentalIntervention;
-//
-//
-//
-//    public BloodType getBloodType() {
-//        return bloodType;
-//    }
-//
-//    public void setBloodType(BloodType bloodType) {
-//        this.bloodType = bloodType;
-//    }
-//
-//    public boolean isPatientArrived() {
-//        return patientArrived;
-//    }
-//
-//    public void setPatientArrived(boolean patientArrived) {
-//        this.patientArrived = patientArrived;
-//    }
-//
-//    public boolean isDeniedType() {
-//        return deniedType;
-//    }
-//
-//    public void setDeniedType(boolean deniedType) {
-//        this.deniedType = deniedType;
-//    }
-//
-//    public String getReasonForDenied() {
-//        return reasonForDenied;
-//    }
-//
-//    public void setReasonForDenied(String reasonForDenied) {
-//        this.reasonForDenied = reasonForDenied;
-//    }
-//
-//    public double getAmountOfBlood() {
-//        return amountOfBlood;
-//    }
-//
-//    public void setAmountOfBlood(double amountOfBlood) {
-//        this.amountOfBlood = amountOfBlood;
-//    }
-//
-//    public boolean isWeight() {
-//        return weight;
-//    }
-//
-//    public void setWeight(boolean weight) {
-//        this.weight = weight;
-//    }
-//
-//    public boolean isSickness() {
-//        return sickness;
-//    }
-//
-//    public void setSickness(boolean sickness) {
-//        this.sickness = sickness;
-//    }
-//
-//    public boolean isInfection() {
-//        return infection;
-//    }
-//
-//    public void setInfection(boolean infection) {
-//        this.infection = infection;
-//    }
-//
-//    public boolean isPressure() {
-//        return pressure;
-//    }
-//
-//    public void setPressure(boolean pressure) {
-//        this.pressure = pressure;
-//    }
-//
-//    public boolean isTherapy() {
-//        return therapy;
-//    }
-//
-//    public void setTherapy(boolean therapy) {
-//        this.therapy = therapy;
-//    }
-//
-//    public boolean isCycle() {
-//        return cycle;
-//    }
-//
-//    public void setCycle(boolean cycle) {
-//        this.cycle = cycle;
-//    }
-//
-//    public boolean isDentalIntervention() {
-//        return dentalIntervention;
-//    }
-//
-//    public void setDentalIntervention(boolean dentalIntervention) {
-//        this.dentalIntervention = dentalIntervention;
-//    }
-//
+    @OneToOne
+    @JoinColumn(name = "blood_id")
+    private Blood blood;
+
+    private boolean weight;
+
+    private boolean sickness;
+
+    private boolean infection;
+
+    private boolean pressure;
+
+    private boolean therapy;
+
+    private boolean cycle;
+
+    private boolean dentalIntervention;
+
+
+
+    public boolean isWeight() {
+        return weight;
+    }
+
+    public void setWeight(boolean weight) {
+        this.weight = weight;
+    }
+
+    public boolean isSickness() {
+        return sickness;
+    }
+
+    public void setSickness(boolean sickness) {
+        this.sickness = sickness;
+    }
+
+    public boolean isInfection() {
+        return infection;
+    }
+
+    public void setInfection(boolean infection) {
+        this.infection = infection;
+    }
+
+    public boolean isPressure() {
+        return pressure;
+    }
+
+    public void setPressure(boolean pressure) {
+        this.pressure = pressure;
+    }
+
+    public boolean isTherapy() {
+        return therapy;
+    }
+
+    public void setTherapy(boolean therapy) {
+        this.therapy = therapy;
+    }
+
+    public boolean isCycle() {
+        return cycle;
+    }
+
+    public void setCycle(boolean cycle) {
+        this.cycle = cycle;
+    }
+
+    public boolean isDentalIntervention() {
+        return dentalIntervention;
+    }
+
+    public void setDentalIntervention(boolean dentalIntervention) {
+        this.dentalIntervention = dentalIntervention;
+    }
 }
--- ISA-projekat/src/main/resources/db/migration/V1_1__create.sql
+++ ISA-projekat/src/main/resources/db/migration/V1_1__create.sql
@@ -45,7 +45,7 @@ CREATE TABLE `user`
     `occupation`        varchar(255),
     `occupation_info`   varchar(255),
     `center_account_id` bigint(20),
-    `points` decimal(19, 4),
+    `points`            decimal(19, 4),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`center_account_id`) REFERENCES center_account (id)
 );
@@ -53,26 +53,34 @@ CREATE TABLE `user`
 DROP TABLE IF EXISTS `poll`;
 CREATE TABLE `poll`
 (
-    `id`           bigint(20) NOT NULL AUTO_INCREMENT,
-    `date_created` datetime   NOT NULL,
-    `date_updated` datetime DEFAULT NULL,
-    `deleted`      bit(1)     NOT NULL,
+    `id`                  bigint(20) NOT NULL AUTO_INCREMENT,
+    `date_created`        datetime   NOT NULL,
+    `date_updated`        datetime DEFAULT NULL,
+    `deleted`             bit(1)     NOT NULL,
+    `blood_id`            bigint(20),
+    `weight`              bit(1),
+    `sickness`            bit(1),
+    `infection`           bit(1),
+    `pressure`            bit(1),
+    `therapy`             bit(1),
+    `cycle`               bit(1),
+    `dental_intervention` bit(1),
     PRIMARY KEY (`id`)
 );
 
 DROP TABLE IF EXISTS `appointment`;
 CREATE TABLE `appointment`
 (
-    `id`            bigint(20) NOT NULL AUTO_INCREMENT,
-    `date_created`  datetime   NOT NULL,
-    `date_updated`  datetime DEFAULT NULL,
-    `deleted`       bit(1)     NOT NULL,
-    `date_and_time` datetime,
-    `admin_id`      bigint(20),
-    `duration`      bigint(20),
-    `patient_id`    bigint(20),
-    `poll_id`       bigint(20),
-    `center_account_id` bigint(20),
+    `id`                    bigint(20) NOT NULL AUTO_INCREMENT,
+    `date_created`          datetime   NOT NULL,
+    `date_updated`          datetime DEFAULT NULL,
+    `deleted`               bit(1)     NOT NULL,
+    `date_and_time`         datetime,
+    `admin_id`              bigint(20),
+    `duration`              bigint(20),
+    `patient_id`            bigint(20),
+    `poll_id`               bigint(20),
+    `center_account_id`     bigint(20),
     `completed_appointment` bit(1),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`admin_id`) REFERENCES User (id),
@@ -99,84 +107,106 @@ CREATE TABLE `feedback`
 
 CREATE TABLE `blood`
 (
-    `id`             bigint(20) NOT NULL AUTO_INCREMENT,
-    `date_created`   datetime   NOT NULL,
-    `date_updated`   datetime DEFAULT NULL,
-    `deleted`        bit(1)     NOT NULL,
-    `blood_type` varchar(255),
-    `amount` decimal(19,4),
+    `id`                bigint(20) NOT NULL AUTO_INCREMENT,
+    `date_created`      datetime   NOT NULL,
+    `date_updated`      datetime DEFAULT NULL,
+    `deleted`           bit(1)     NOT NULL,
+    `blood_type`        varchar(255),
+    `amount`            decimal(19, 4),
     `center_account_id` bigint(20),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`center_account_id`) REFERENCES center_account (id)
 );
 
 insert into `user`(date_created, date_updated, deleted, email, first_name, password, address, country, city, phone,
-                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,points)
+                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,
+                   points)
 VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'nebojsa@gmail.com', 'Nebojsa',
         '$2a$10$36dVOozCi/zxI01Lph5KVODLdutdC7LKbRj/YHU7uz23eRxgxM.na', 'a', 'c', 'c', 'p', 'ADMIN_CENTER',
-        'Bogosavljev', false, '3213213', 'MALE', '', '', 1,0);
+        'Bogosavljev', false, '3213213', 'MALE', '', '', 1, 0);
 
 insert into `user`(date_created, date_updated, deleted, email, first_name, password, address, country, city, phone,
-                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,points)
-    VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'test@gmail.com', 'Marko',
-            '$2a$10$36dVOozCi/zxI01Lph5KVODLdutdC7LKbRj/YHU7uz23eRxgxM.na', 'a', 'c', 'c', 'p', 'USER',
-            'Markovic', false, '3213213', 'MALE', '', '', null,0);
+                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,
+                   points)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'test@gmail.com', 'Marko',
+        '$2a$10$36dVOozCi/zxI01Lph5KVODLdutdC7LKbRj/YHU7uz23eRxgxM.na', 'a', 'c', 'c', 'p', 'USER',
+        'Markovic', false, '3213213', 'MALE', '', '', null, 0);
 
 insert into `user`(date_created, date_updated, deleted, email, first_name, password, address, country, city, phone,
-                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,points)
+                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,
+                   points)
 VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'test2@gmail.com', 'Bojan',
         '$2a$10$36dVOozCi/zxI01Lph5KVODLdutdC7LKbRj/YHU7uz23eRxgxM.na', 'a', 'c', 'c', 'p', 'USER',
-        'Braun', false, '3213213', 'MALE', '', '', null,0);
+        'Braun', false, '3213213', 'MALE', '', '', null, 0);
 
 insert into `user`(date_created, date_updated, deleted, email, first_name, password, address, country, city, phone,
-                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,points)
+                   role, last_name, first_login, personal_id, gender, occupation, occupation_info, center_account_id,
+                   points)
 VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'test3@gmail.com', 'Igor',
         '$2a$10$36dVOozCi/zxI01Lph5KVODLdutdC7LKbRj/YHU7uz23eRxgxM.na', 'a', 'c', 'c', 'p', 'USER',
-        'igic', false, '3213213', 'MALE', '', '', null,0);
+        'igic', false, '3213213', 'MALE', '', '', null, 0);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'A',11,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'A', 11, 1);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'A',2,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'A', 2, 1);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'B',3,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'B', 3, 1);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'B',4,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'B', 4, 1);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'AB',1,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'AB', 1, 1);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'AB',2,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'AB', 2, 1);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'ZERO',2,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'ZERO', 2, 1);
 
 insert into `blood`(date_created, date_updated, deleted, blood_type, amount, center_account_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'ZERO',3,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 'ZERO', 3, 1);
 
-insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id, center_account_id, completed_appointment)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'2023-04-22 10:34:23',1,12,2,null,1,true);
+insert into `poll`(date_created, date_updated, deleted, blood_id, weight, sickness, infection,
+                   pressure, therapy, cycle, dental_intervention)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 1, true, true, true, true, true, true, true);
 
-insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id, center_account_id, completed_appointment)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'2023-06-22 10:34:23',1,12,3,null,1,true);
+insert into `poll`(date_created, date_updated, deleted, blood_id, weight, sickness, infection,
+                   pressure, therapy, cycle, dental_intervention)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 2, false, false, false, false, false, false,
+        false);
 
-insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id, center_account_id, completed_appointment)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'2023-05-22 10:34:23',1,12,4,null,1,true);
+insert into `poll`(date_created, date_updated, deleted, blood_id, weight, sickness, infection,
+                   pressure, therapy, cycle, dental_intervention)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 3, true, true, true, true, true, true, false);
 
-insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id, center_account_id, completed_appointment)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,'2023-05-22 10:34:23',1,12,4,null,1,false);
+insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id,
+                          center_account_id, completed_appointment)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, '2023-04-22 10:34:23', 1, 12, 2, 1, 1, true);
 
-insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id, center_account_id, completed_appointment)
-VALUES ('2022-04-22 10:34:23', '2022-04-11 10:34:23	', false,'2023-05-22 10:34:23',1,12,4,null,1,false);
+insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id,
+                          center_account_id, completed_appointment)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, '2023-06-22 10:34:23', 1, 12, 3, 2, 1, true);
+
+insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id,
+                          center_account_id, completed_appointment)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, '2023-05-22 10:34:23', 1, 12, 4, 3, 1, true);
+
+insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id,
+                          center_account_id, completed_appointment)
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, '2023-05-22 10:34:23', 1, 12, 4, 1, 1, false);
+
+insert into `appointment`(date_created, date_updated, deleted, date_and_time, admin_id, duration, patient_id, poll_id,
+                          center_account_id, completed_appointment)
+VALUES ('2022-04-22 10:34:23', '2022-04-11 10:34:23	', false, '2023-05-22 10:34:23', 1, 12, 4, 2, 1, false);
 
 insert into `feedback`(date_created, date_updated, deleted, grade, comment, user_id, appointment_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,5,'bla',2,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 5, 'bla', 2, 1);
 
 insert into `feedback`(date_created, date_updated, deleted, grade, comment, user_id, appointment_id)
-VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false,11,'bla',2,1);
+VALUES ('2022-04-22 10:34:23', '2022-04-22 10:34:23	', false, 11, 'bla', 2, 1);
 
 
